ce66b4250c33e1fc860331b06f3fad084fbe478d
emaphis <emaphis85@gmail.com>
2024-12-03 20:44:04 -0500

    Finished Day 3 Part 2 - Mull It Over

--- AOC2024_FSharp/Day03/Day03Part2.fsx
+++ AOC2024_FSharp/Day03/Day03Part2.fsx
@@ -3,27 +3,29 @@
 #r "nuget: Unquote"
 
 open System.IO
-open Swensen.Unquote
 open System.Text.RegularExpressions
+open Swensen.Unquote
 
 /// Get data from the `..\data` directory and store it in a list of string
 /// Data file should have only one item.
 let getData (fileName: string) =
     File.ReadAllText $"""{__SOURCE_DIRECTORY__}\..\Data\{fileName}"""
 
-let test03 = getData "test03.txt"
+let test03 = getData "test03b.txt"
 let data03 = getData "data03.txt"
 
 
-// Part 1
+// Part 2
 
 /// Toke DU for a simple three instruction language
 type Token =
+    | Do
+    | Dont
     | Mul of int * int
 
 /// Parse data returning tokes.
 let parseTokens (input: string) =
-    let regex = @"(mul)\((?:(\d+),(\d+))?\)"
+    let regex = @"(mul|do|don't)\((?:(\d+),(\d+))?\)"
     let rgx = Regex(regex)
     let data = rgx.Matches(input)
 
@@ -32,6 +34,8 @@ let parseTokens (input: string) =
         match mtch.Groups[1].Value with
         | "mul" ->
             (int mtch.Groups[2].Value, int mtch.Groups[3].Value) |> Mul
+        | "do" -> Do
+        | "don't" -> Dont
         | _ -> failwith "Bad token")
 
 
@@ -43,31 +47,37 @@ let parseTokens (input: string) =
 
 let processTokens (tokens: Token seq) =
     tokens
-    |>Seq.map (fun tkns ->
+    |>Seq.fold (fun (sum, domult) tkns ->
     match tkns with
-    | Mul (x, y) -> x * y
-//    | _ -> 0
-    )
-    |> Seq.sum
+    | Do  -> (sum, true)
+    | Dont -> (sum, false)
+    | Mul (x, y) ->
+        let newSum =
+            match domult with
+            | true -> x * y + sum
+            | false -> sum
+        (newSum, domult)
+    ) (0, true)
+    |> fst
 
-//processTokens testTokens  // 161
+//processTokens testTokens  // 48
 
 
 let part2 input =
     let data = parseTokens input
     processTokens data
 
-let part01Test = part2 test03  // 161
-let part01Ans = part2  data03  // 174561379
+let part01Test = part2 test03  // 48
+let part01Ans = part2  data03  // 106921067
 
-// Answer: 174561379
+// Answer: 106921067
 
 
 // Tests and examples
 let tests () =
     printf "Testing.."
-    test <@ part2 test03 = 161 @>
-    test <@ part2 data03 = 174561379 @>
+    test <@ part2 test03 = 48 @>
+    test <@ part2 data03 = 106921067 @>
     printfn "...done!"
 
 do tests ()
--- AOC2024_FSharp/Day03/README.md
+++ AOC2024_FSharp/Day03/README.md
@@ -1,3 +1,11 @@
 # Day 03 - Mull It Over
 
-An exercise in regular expressions
+An exercise in regular expressions and simple langage interpreters.
+
+Part 2 is basically a three instruction language
+
+mul(9,9)  // A multiplication express
+do        // expression evaluator
+don't     // essentially a comment.
+
+To match Part 2 I redid Part 1 as a single expression language
